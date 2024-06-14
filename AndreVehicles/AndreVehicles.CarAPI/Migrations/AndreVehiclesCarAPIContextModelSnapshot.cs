b0ad57748f0537fe588fd5aa509e01fdf3560e48
gutsserrano <gutsserrano@gmail.com>
2024-06-14 10:44:25 -0300

    feat: creating DriverAPI

--- AndreVehicles/AndreVehicles.CarAPI/Migrations/AndreVehiclesCarAPIContextModelSnapshot.cs
+++ AndreVehicles/AndreVehicles.CarAPI/Migrations/AndreVehiclesCarAPIContextModelSnapshot.cs
@@ -153,6 +153,57 @@ namespace AndreVehicles.CarAPI.Migrations
                     b.ToTable("FinancialPending", (string)null);
                 });
 
+            modelBuilder.Entity("Models.Insurances.DriversLicense", b =>
+                {
+                    b.Property<long>("License")
+                        .HasColumnType("bigint");
+
+                    b.Property<long>("CategoryId")
+                        .HasColumnType("bigint");
+
+                    b.Property<string>("Cpf")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("DueDate")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("FatherName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("MotherName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Rg")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("License");
+
+                    b.HasIndex("CategoryId");
+
+                    b.ToTable("DriversLicense", (string)null);
+                });
+
+            modelBuilder.Entity("Models.Insurances.DriversLicenseCategory", b =>
+                {
+                    b.Property<long>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("bigint");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<long>("Id"), 1L, 1);
+
+                    b.Property<string>("Description")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("DriversLicenseCategory");
+                });
+
             modelBuilder.Entity("Models.People.Address", b =>
                 {
                     b.Property<string>("Id")
@@ -395,6 +446,18 @@ namespace AndreVehicles.CarAPI.Migrations
                     b.ToTable("Sale", (string)null);
                 });
 
+            modelBuilder.Entity("Models.Insurances.Driver", b =>
+                {
+                    b.HasBaseType("Models.People.Person");
+
+                    b.Property<long>("License1")
+                        .HasColumnType("bigint");
+
+                    b.HasIndex("License1");
+
+                    b.ToTable("Driver", (string)null);
+                });
+
             modelBuilder.Entity("Models.People.Customer", b =>
                 {
                     b.HasBaseType("Models.People.Person");
@@ -477,6 +540,17 @@ namespace AndreVehicles.CarAPI.Migrations
                     b.Navigation("Customer");
                 });
 
+            modelBuilder.Entity("Models.Insurances.DriversLicense", b =>
+                {
+                    b.HasOne("Models.Insurances.DriversLicenseCategory", "Category")
+                        .WithMany()
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Category");
+                });
+
             modelBuilder.Entity("Models.People.Person", b =>
                 {
                     b.HasOne("Models.People.Address", "Address")
@@ -553,6 +627,23 @@ namespace AndreVehicles.CarAPI.Migrations
                     b.Navigation("Employee");
 
                     b.Navigation("Payment");
+                });
+
+            modelBuilder.Entity("Models.Insurances.Driver", b =>
+                {
+                    b.HasOne("Models.People.Person", null)
+                        .WithOne()
+                        .HasForeignKey("Models.Insurances.Driver", "Document")
+                        .OnDelete(DeleteBehavior.ClientCascade)
+                        .IsRequired();
+
+                    b.HasOne("Models.Insurances.DriversLicense", "License")
+                        .WithMany()
+                        .HasForeignKey("License1")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("License");
                 });
 
             modelBuilder.Entity("Models.People.Customer", b =>
